35ea62e0e1b38bf9ad84eda585a9641a913f1e1c
VLAD <dv.vladislav98@gmail.com>
2017-03-14 09:52:08 +0300

    PHP Laba №4

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+ <head>
+  <title>PHP</title>
+ </head>
+ <body>
+    <?php echo '<p>Hello world!</p>'; ?>
+    <?php 
+        $dblocation = "localhost";
+        $dbname = "carshowroom";
+        $dbuser = "root";
+        $dbpass = "";
+        $dbcnx = @mysql_connect($dblocation, $dbuser, $dbpass);
+        if (!$dbcnx) {
+            echo "No connection ";
+            exit();
+        }
+        if(!@mysql_select_db($dbname, $dbcnx)) {
+            echo "No";
+            exit();
+        }
+        $ver = mysql_query("select version()");
+        if(!$ver){
+            echo "error";
+            exit();
+        }
+        
+        $selectCar = mysql_query("select * from car");
+        if(! $selectCar){
+            throw new My_Db_Exception('Database error: ' . mysql_error());
+        }
+        while($car = mysql_fetch_assoc($selectCar)) {
+            echo "ID: " . $car["ID"]. " - Country: " . $car["Country"]. " - Marka: " . $car["Marka"] . " - Model: " . $car["Model"] . "<br>";
+        }
+    ?>
+ </body>
+</html>
